3658d1f2a9eba06f63f4e757afebb545693a7ad2
Muhammad Iqbal Pranatayudha <pranatayudha001@gmail.com>
2024-06-01 00:57:56 +0700

    server:fix(db): Change database conenction URI in Sequelize instance

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,8 @@
 const Sequelize = require("sequelize");
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
+  process.env.DATABASE_URL ||
+    "postgres://postgres:p455w0rd@localhost:5432/messenger",
   {
     logging: false,
   }
