96ae95ab6b8abde79bb4980fd1d3b4b2daec4228
Muhammad Iqbal Pranatayudha <pranatayudha001@gmail.com>
2024-06-02 14:46:01 +0700

    server:feat(messages): Add service for updating message read status

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { Conversation, Message } = require("../../db/models");
 const onlineUsers = require("../../onlineUsers");
+const db = require("../../db");
+const { Op } = require("sequelize");
 
 // expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
 router.post("/", async (req, res, next) => {
@@ -45,6 +47,94 @@ router.post("/", async (req, res, next) => {
     });
     res.json({ message, sender });
   } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:conversationId", async (req, res, next) => {
+  const transaction = await db.transaction();
+
+  try {
+    if (!req.user) {
+      await transaction.rollback();
+
+      return res.sendStatus(401);
+    }
+
+    const userId = req.user.id;
+    const conversationId = req.params.conversationId;
+
+    let conversation = await Conversation.findOne(
+      {
+        where: {
+          id: conversationId,
+        },
+      },
+      { transaction }
+    );
+
+    if (!conversation) {
+      await transaction.rollback();
+
+      res.status(404).json({ error: "Conversation not found" });
+    }
+
+    let message = await Message.findAll(
+      {
+        where: {
+          conversationId,
+        },
+      },
+      { transaction }
+    );
+
+    if (!message) {
+      await transaction.rollback();
+
+      res.status(404).json({ error: "Message not found" });
+    }
+
+    if (message.filter((m) => !m.hasRead).length > 0) {
+      conversation = await Conversation.findOne(
+        {
+          where: {
+            id: conversationId,
+            [Op.or]: {
+              user1Id: userId,
+              user2Id: userId,
+            },
+          },
+        },
+        { transaction }
+      );
+
+      if (conversation) {
+        const updateMessage = await Message.update(
+          { hasRead: true },
+          {
+            where: {
+              conversationId,
+            },
+          },
+          { transaction }
+        );
+
+        message = await Message.findOne(
+          {
+            where: {
+              id: updateMessage,
+            },
+          },
+          { transaction }
+        );
+      }
+    }
+
+    await transaction.commit();
+
+    res.json({ message });
+  } catch (error) {
+    await transaction.rollback();
     next(error);
   }
 });
